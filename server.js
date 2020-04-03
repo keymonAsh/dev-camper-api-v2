a93b67c826adf86dc4213d513a6e6a5f755eb2c5
Aswin Gangadharan <keymonash9@gmail.com>
2020-04-03 08:59:46 +0530

    fix unhandeled promises

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const colors = require('colors')
 
 // Connecting to Database
 const connetDB = require('./db')
@@ -13,4 +14,10 @@ const bootcamps = require('./routes/bootcamps')
 // use Routes
 app.use('/api/bootcamps', bootcamps)
 
-app.listen(process.env.PORT, console.log("Server Live"))
+const server = app.listen(process.env.PORT, console.log("Server Live".blue))
+
+// Unhandled Promises
+process.on("unhandledRejection", err => {
+    console.log(`Promise rejection error: ${err}`.red)
+    server.close(() => process.exit(1))
+})
