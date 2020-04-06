9022b719850d1c76c36588778a0b5fa017b61817
Aswin Gangadharan <keymonash9@gmail.com>
2020-04-06 15:21:27 +0530

    login route

--- handlers/auth.js
+++ handlers/auth.js
@@ -3,11 +3,38 @@ const asyncHandler = require('../middleware/async')
 const User = require('../models/User')
 
 // desc: Register User
-// route: POST /api/bootcamps
+// route: POST /api/register
 // access: Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body
     const user = await User.create({ name, email, password, role })
+
+    // create token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({ success: true, token })
+}) 
+
+// desc: Login User
+// route: POST /api/login
+// access: Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+
+    // ## Validation
+    if(!email || !password) {
+        return next(new ErrorResponse("Please provide email and password", 400))
+    }
+    // checking user
+    const user = await User.findOne({ email }).select('+password')
+    if(!user) {
+        return next(new ErrorResponse("invalid Credentials", 401))
+    }
+    // checking password
+    const isMatch = await user.matchPassword(password)
+    if(!isMatch) {
+        return next(new ErrorResponse("invalid Credentials", 401))
+    }
 
     // create token
     const token = user.getSignedJwtToken()
--- models/User.js
+++ models/User.js
@@ -49,4 +49,9 @@ userSchema.methods.getSignedJwtToken = function() {
     })
 }
 
+// checking hashed password for login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
 module.exports = mongoose.model('User', userSchema)
