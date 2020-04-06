c7bcde8cbdc2f234706349f1a7bc26853ea3a25f
Aswin Gangadharan <keymonash9@gmail.com>
2020-04-06 13:57:36 +0530

    Jwt token

--- handlers/auth.js
+++ handlers/auth.js
@@ -7,8 +7,10 @@ const User = require('../models/User')
 // access: Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body
-
     const user = await User.create({ name, email, password, role })
 
-    res.status(200).json({ success: true })
+    // create token
+    const token = user.getSignedJwtToken()
+
+    res.status(200).json({ success: true, token })
 }) 
